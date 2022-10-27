32a15b282a7b5b6dbbb5819de3518cdd2fa50557
Juan Carlos M Giner <jcmginer@gmail.com>
2022-10-27 16:04:44 +0200

    Modified App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Products from './components/Products/Products';
+import TopNav from './components/TopNav/TopNav';
+import TopSearch from './components/TopSearch/TopSearch';
 
 function App() {
   return (
     <>
-    
+    <TopNav />
+    <TopSearch />
+    <Products />
+    <Footer />
     </>
   );
 }
