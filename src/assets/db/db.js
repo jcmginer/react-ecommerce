d2fd3ba538e652a689f69a900b2d9d43f30919c0
Juan Carlos M Giner <jcmginer@gmail.com>
2022-10-27 16:01:47 +0200

    Added assets

---
+++ src/assets/db/db.js
@@ -0,0 +1,22 @@
+export const CardsInfo = [
+  {
+    name: 'Black BP',
+    price: '49,99$',
+    img: '01'
+  },
+  {
+    name: 'Dune BP',
+    price: '62,99$',
+    img: '02'
+  },
+  {
+    name: 'Black Camouflage BP',
+    price: '69,99$',
+    img: '03'
+  },
+  {
+    name: 'Moss BP',
+    price: '49,99$',
+    img: '03'
+  }
+]
---
+++ src/assets/img/index.js
@@ -0,0 +1,13 @@
+import { default as photo01 } from './img-pro-01.jpeg';
+import { default as photo02 } from './img-pro-02.jpeg';
+import { default as photo03 } from './img-pro-03.jpeg';
+import { default as photo04 } from './img-pro-04.jpeg';
+import { default as logoHeader } from './logo.png';
+
+export {
+  photo01,
+  photo02,
+  photo03,
+  photo04,
+  logoHeader
+}
