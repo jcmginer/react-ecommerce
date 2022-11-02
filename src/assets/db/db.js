1e924bb67a37dd22d1cee81daf85d44e255679dd
Juan Carlos M Giner <jcmginer@gmail.com>
2022-11-02 13:46:39 +0100

    Modified db

--- src/assets/db/db.js
+++ src/assets/db/db.js
@@ -2,21 +2,41 @@ export const CardsInfo = [
   {
     name: 'Black BP',
     price: '49,99$',
-    img: '01'
+    img: 'photo01'
   },
   {
     name: 'Dune BP',
     price: '62,99$',
-    img: '02'
+    img: 'photo02'
   },
   {
     name: 'Black Camouflage BP',
     price: '69,99$',
-    img: '03'
+    img: 'photo03'
   },
   {
     name: 'Moss BP',
     price: '49,99$',
-    img: '04'
+    img: 'photo04'
+  },
+  {
+    name: 'Black BP',
+    price: '49,99$',
+    img: 'photo01'
+  },
+  {
+    name: 'Dune BP',
+    price: '62,99$',
+    img: 'photo02'
+  },
+  {
+    name: 'Black Camouflage BP',
+    price: '69,99$',
+    img: 'photo03'
+  },
+  {
+    name: 'Moss BP',
+    price: '49,99$',
+    img: 'photo04'
   }
 ]
